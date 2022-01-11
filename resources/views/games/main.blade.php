7a55df8d7e6ea2b9d3e4baa104a6f5c798bd85a3
LathiefSw <lathiefsentiko076@gmail.com>
2022-01-11 15:44:20 +0700

    Loginlogout

--- resources/views/games/main.blade.php
+++ resources/views/games/main.blade.php
@@ -74,16 +74,51 @@
                                     <a class="nav-link" href="logout.php">Log Out</a>
                                 </li>
                             </ul>
+                            <ul class="navbar-nav ml-auto">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }} <span class="caret"></span>
+                                        </a>
+        
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                               onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+        
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
                         </div>
                     </div>
                 </nav>
+
             <section class="gambar">
                     <div class="card-body">
                         <div class="container">
-                            <h2><center><strong>ROM GAME PLAYSTATIONS</strong></center><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br></h2>
+                            <h2><center><strong>ROM GAME PLAYSTATIONS</strong></center><br><br><br><br><br><br><br><br><br><br><br></h2>
                         </div>
                     </div>
             </section>
+            <main class="py-4">
+                @yield('content')
+            </main>
                 <br>
                 <footer>
                     <div class = "text-light text-center" >
